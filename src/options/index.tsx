ea8553122c0e5de122d2651fc7a52c4fdf040887
Will O'Beirne <wbobeirne@gmail.com>
2018-11-12 14:57:57 -0500

    Add options page. Minor styles for it.

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,1 +1,11 @@
-alert('Not yet implemented');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from '../app/index';
+import Routes from '../app/AppRoutes';
+
+import './index.less';
+
+ReactDOM.render(
+  <App routes={<Routes />} /> as any,
+  document.getElementById('root') as HTMLElement,
+);
