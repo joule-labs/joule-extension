47b7fa76d63a9b9b41e4b9d48655f477d3f3d38e
Aleksey Bykhun <theisease@gmail.com>
2018-11-16 16:50:24 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -111,7 +111,7 @@ module.exports = {
           },
           {
             loader: 'ts-loader',
-            options: { transpileOnly: isDev },
+            options: { transpileOnly: true },
           },
         ],
       },
