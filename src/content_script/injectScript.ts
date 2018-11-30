9b50eef974aefd3301bf188b06d19fbcece6b23c
Will O'Beirne <wbobeirne@gmail.com>
2018-11-29 22:11:03 -0500

    Document start, inject into document or head.

--- src/content_script/injectScript.ts
+++ src/content_script/injectScript.ts
@@ -3,9 +3,10 @@ import { browser } from 'webextension-polyfill-ts';
 export default function injectScript() {
   try {
     if (!document) throw new Error('No document');
-    const container = document.body;
+    const container = document.head || document.documentElement;
     if (!container) throw new Error('No container element');
     const scriptEl = document.createElement('script');
+    scriptEl.setAttribute('async', 'false');
     scriptEl.setAttribute('type', 'text/javascript');
     scriptEl.setAttribute('src', browser.extension.getURL('inpage_script.js'));
     container.appendChild(scriptEl);
