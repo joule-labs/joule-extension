e96a48e47b8361119ed7002e6f5d001cd0e86325
Michael Rodriguez <southsideprod@hotmail.com>
2019-01-14 13:19:02 -0500

    blockchainLogos type to React.ReactNode

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,7 +1,6 @@
 import BitcoinLogo from 'static/images/bitcoin.svg';
 import LitecoinLogo from 'static/images/litecoin.svg';
 import * as React from 'react';
-import { CustomIconComponentProps } from 'antd/lib/icon';
 
 export const DEFAULT_LOCAL_NODE_URLS = [
   'https://localhost:8080',
@@ -23,7 +22,7 @@ export const coinSymbols: { [key in CHAIN_TYPE]: string } = {
   litecoin: 'LTC'
 };
 
-export const blockchainLogos: { [key in CHAIN_TYPE]: React.ComponentType<CustomIconComponentProps> } = {
+export const blockchainLogos: { [key in CHAIN_TYPE]: React.ReactNode } = {
   bitcoin: BitcoinLogo,
   litecoin: LitecoinLogo,
 };
