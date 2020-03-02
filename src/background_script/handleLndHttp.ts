81ceec611d5a55230b92e39fb8b843655d500cdf
Will O'Beirne <wbobeirne@gmail.com>
2020-03-01 21:00:53 -0600

    Fix background http proxy responding to all messages

--- src/background_script/handleLndHttp.ts
+++ src/background_script/handleLndHttp.ts
@@ -16,29 +16,33 @@ function isLndRequestMessage(req: any): req is LndAPIRequestMessage<LndAPIMethod
 
 export default function handleLndHttp() {
   // Background manages communication between page and its windows
-  browser.runtime.onMessage.addListener(async (request: unknown) => {
+  browser.runtime.onMessage.addListener((request: unknown) => {
     if (!isLndRequestMessage(request)) {
       return;
     }
 
-    const client = new LndHttpClient(request.url, request.macaroon);
-    const fn = client[request.method] as LndHttpClient[typeof request.method];
-    const args = request.args as Parameters<LndHttpClient[typeof request.method]>;
+    return new Promise(resolve => {
+      const client = new LndHttpClient(request.url, request.macaroon);
+      const fn = client[request.method] as LndHttpClient[typeof request.method];
+      const args = request.args as Parameters<LndHttpClient[typeof request.method]>;
 
-    return (fn as any)(...args)
-      .then((data: ReturnType<LndHttpClient[typeof request.method]>) => {
-        return {
-          type: 'lnd-api-response',
-          method: request.method,
-          data,
-        } as LndAPIResponseMessage<typeof request.method>;
-      })
-      .catch((err: LndAPIResponseError) => {
-        return {
-          type: 'lnd-api-response',
-          method: request.method,
-          error: err,
-        } as LndAPIResponseMessage<typeof request.method>;
-      });
+      resolve(
+        (fn as any)(...args)
+          .then((data: ReturnType<LndHttpClient[typeof request.method]>) => {
+            return {
+              type: 'lnd-api-response',
+              method: request.method,
+              data,
+            } as LndAPIResponseMessage<typeof request.method>;
+          })
+          .catch((err: LndAPIResponseError) => {
+            return {
+              type: 'lnd-api-response',
+              method: request.method,
+              error: err,
+            } as LndAPIResponseMessage<typeof request.method>;
+          }),
+      );
+    });
   });
 }
